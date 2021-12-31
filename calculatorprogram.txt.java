de7ae83f540d35b1234e2f79b67d4ed12bff34e7
vijay <digitalxvijay@gmail.com>
2021-12-31 12:45:59 +0530

    calculator program is added

---
+++ calculatorprogram.txt.java
@@ -0,0 +1,80 @@
+import java.util.Scanner;
+
+public class calculatorprogram {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int ans = 0;
+        while (true) {
+            System.out.println("enter the operator: ");
+            char op = in.next().trim().charAt(0);
+            if (op == '+' || op == '-' || op == '%' || op == '/' || op == '*') {
+                System.out.print("Enter the value of num1: ");
+                int num1 = in.nextInt();
+                System.out.print("Enter the value of num2: ");
+                int num2 = in.nextInt();
+                if (op == '+') {
+                    ans = num1 + num2;
+                }
+                if (op == '-') {
+                    ans = num1 - num2;
+                }
+                if (op == '%') {
+                    ans = num1 % num2;
+                }
+                if (op == '/') {
+                    ans = num1 / num2;
+                }
+
+                if (op == '*') {
+                    ans = num1 * num2;
+                }
+            } else if (op == 'X' || op == 'x') {
+                break;
+
+            } else {
+                System.out.print("Invalid numbers mava");
+
+            }
+            System.out.println(ans);
+
+        }
+    }
+
+}
+/*
+ * char op = in.next().trim().charAt(0);
+ * if (op == '+' || op == '-' || op == '%' || op == '/' || op == '*') {
+ * System.out.print("Enter the value of num1: ");
+ * int num1 = in.nextInt();
+ * System.out.print("Enter the value of num2: ");
+ * int num2 = in.nextInt();
+ * if (op == '+') {
+ * ans = num1 + num2;
+ * }
+ * if (op == '-') {
+ * ans = num1 - num2;
+ * }
+ * if (op == '%') {
+ * ans = num1 % num2;
+ * }
+ * if (op == '/') {
+ * ans =num1/num2;
+ * }
+ * 
+ * if (op == '*') {
+ * ans = num1 * num2;
+ * }
+ * else if (op == 'X' || op == 'x') {
+ * System.out.print("invalid syntax");
+ * break;}
+ * }
+ * 
+ * }
+ * else {
+ * System.out.print("Invalid numbers mava");
+ * 
+ * }
+ * System.out.print(ans);
+ * }
+ * }
+ */
